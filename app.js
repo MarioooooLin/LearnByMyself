c3bfe8a8b219aadab893d8693d7a3614cf1c5fed
Mario <lintt74123@gmail.com>
2023-02-23 15:32:23 +0800

    npm practice

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 // fs (file system)
-const fs = require("fs");
-fs.writeFile("myFile.txt", "Good mood", (e) => {
-    if (e) throw e;
-    console.log("document complete");
-});
+// const fs = require("fs");
+// fs.writeFile("myFile.txt", "Good mood", (e) => {
+//     if (e) throw e;
+//     console.log("document complete");
+// });
 
-fs.readFile("myFile.txt", "utf-8", (e, data) => {
-    if (e) throw e;
-    console.log(data);
-});
+// fs.readFile("myFile.txt", "utf-8", (e, data) => {
+//     if (e) throw e;
+//     console.log(data);
+// });
+var cowsay = require("cowsay");
+
+console.log(
+    cowsay.say({
+        text: "喔喔喔喔喔喔喔喔喔",
+        e: "$ $",
+        T: "U ",
+    })
+);
