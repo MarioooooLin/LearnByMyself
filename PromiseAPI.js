b0a59e0d3352b843ba3b91e5460883aef5f3e4e5
Mariooooo <lintt74123@gmail.com>
2023-03-03 22:31:22 +0800

    Promise API practice

---
+++ PromiseAPI.js
@@ -0,0 +1,8 @@
+const name = document.querySelector("#name");
+const delay = document.querySelector("#delay");
+const set = document.querySelector("#set");
+const output = document.querySelector("#output");
+
+set.addEventListener("click", (e) => {
+    console.log("Your clicking the button");
+});
