1007129d53b1ca689fcc9067042ba71fec6a3a58
Mariooooo <lintt74123@gmail.com>
2023-03-10 02:39:25 +0800

    EJS information render practice

--- app.js
+++ app.js
@@ -4,15 +4,23 @@ const app = express();
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-app.get("/:name", (req, res) => {
-    let { name } = req.params;
-
-    res.render("index", { name });
-});
+// app.get("/:name", (req, res) => {
+//     let { name } = req.params;
+//     res.render("index", { name });
+// });
 
 app.get("/", (req, res) => {
-    let string = "<h1>Hello world</h1>";
-    res.render("index", { string });
+    const practice = [
+        { name: "Mario", age: 25, Live: "KH" },
+        { name: "Maria", age: 15, Live: "TP" },
+        { name: "Max", age: 45, Live: "TN" },
+    ];
+    res.render("index", { practice });
+});
+
+app.get("/example", (req, res) => {
+    let { name, age } = req.query;
+    res.render("response", { name, age });
 });
 
 app.listen(4000, () => {
