f489cf35d8640101201f647d2682c7891b9de2fc
Mario <lintt74123@gmail.com>
2023-03-22 23:45:37 +0800

    IIFE practice

---
+++ IIFE.js
@@ -0,0 +1,9 @@
+(function hello() {
+    console.log("Hello");
+})();
+
+let hello = "Hello World!"; // Variable 'hello' can use again
+
+(function () {
+    console.log("Hi!");
+})(); // function name could be dismiss
