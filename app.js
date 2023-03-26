70966341027e7914a4dde4d6b39288126003cd03
Mariooooo <lintt74123@gmail.com>
2023-03-27 00:56:48 +0800

    Use cors

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ mongoose
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors);
+app.use(cors());
 
 app.get("/students", async (req, res) => {
     try {
